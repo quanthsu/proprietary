6c4c0f64b17c2d00298dabe8706743e999d3019e
twhsu <twhsu@nlg.csie.ntu.edu.tw>
2023-05-19 11:06:18 +0800

    feat: revise orders_trades_update

--- orders_trades_update.py
+++ orders_trades_update.py
@@ -4,49 +4,62 @@ import datetime as dt
 from tsdb_client import TSDBClient
 from utils.Config import EnvConfig
 
+cli = TSDBClient(
+        host=EnvConfig.DB_HOST,
+        port=EnvConfig.DB_PORT,
+        user=EnvConfig.DB_USER,
+        password=EnvConfig.DB_PASSWORD,
+        db=EnvConfig.DB_DATABASE,
+    )
 
-def time_transform(time):
-    return datetime.datetime.strptime(time, '%H%M%S').time()
+strategy_df = cli.execute_query('''
+        SELECT * FROM dealer.strategy
+        ''', 
+            out_type='df')
 
-def security_type_transform(security_type):
+def time_transform(t):
+    return dt.datetime.strptime(t, '%H%M%S').time()
+
+def security_type_transform(security_type: str):
     if security_type == "現股":
         return 'S'
     elif security_type == "期貨":
         return 'F'
 
-def action_transform(action):
+def action_transform(action: str):
     if action == 'Buy': return 'B'
     if action == 'Sell': return 'S'
         
+
+def strategy_align(log_type: str, strategy_name: str, order: pd.DataFrame):
+
+    log = pd.read_csv(f'print/{strategy_name}/{log_type}.log', header = None, names = ['serial', 'security_type', 'timestamp', 'code', 'order_type', 'action', 'order_qty', 'order_price'])
+
+    for index, row in log.iterrows():
+    
+        cur_order = order[(order['security_type'] == row.security_type[0]) & (order['code'] == str(row.code)) & (order['order_type'] == row.order_type) & 
+        (order['action'] == row.action) & (order['order_price'].astype('float') == row.order_price)]
+        
+        for index in cur_order.index:
+            order.loc[index, 'strategy'] = int(strategy_df[strategy_df['name'] == strategy_name]['id'].values[0])
+    return order
+
 
 def load_orders(path: str):
     return pd.read_csv('status/Order.txt', header = None,
         names = ['trader_id','order_id', 'security_type', 'order_time', 'code', 'order_type', 'action', 'order_qty', 'order_price', 'status'],
         dtype='str')
 
-
 def process_orders(order: pd.DataFrame):
     order['security_type'] = order['security_type'].apply(security_type_transform)
     order['order_time'] = order['order_time'].apply(time_transform)
-    order['order_date'] = datetime.datetime.now().date()
+    order['order_date'] = dt.datetime.now().date()
     order['action'] = order['action'].apply(action_transform)
-
-    strategy_df = cli.execute_query('''
-        SELECT * FROM dealer.strategy
-        ''', 
-            out_type='df')
 
     order['strategy'] = 0
     for strategy_name in strategy_df['name']:
-        buy_log = pd.read_csv(f'print/{strategy_name}/Buy.log', header = None, names = ['serial', 'security_type', 'timestamp', 'code', 'order_type', 'action', 'order_qty', 'order_price'])
-        
-        for index, row in buy_log.iterrows():
-
-            cur_order = order[(order['security_type'] == row.security_type[0]) & (order['code'] == str(row.code)) & (order['order_type'] == row.order_type) & 
-            (order['action'] == row.action) & (order['order_price'].astype('float') == row.order_price)]
-            
-            for index in cur_order.index:
-                order.loc[index, 'strategy'] = int(strategy_df[strategy_df['name'] == strategy_name]['id'].values[0])
+        order = strategy_align("Buy", strategy_name, order)
+        order = strategy_align("Sell", strategy_name, order)
 
     return order
 
@@ -58,24 +71,19 @@ def load_trades(path: str):
 def process_trades(deal: pd.DataFrame, order: pd.DataFrame):
     deal['security_type'] = deal['security_type'].apply(security_type_transform)
     deal['trade_time'] = deal['trade_time'].apply(time_transform)
-    deal['trade_date'] = datetime.datetime.now().date() 
+    deal['trade_date'] = dt.datetime.now().date() 
     deal['action'] = deal['action'].apply(action_transform)
 
-    deal['strategy'] = 0
+    deal['strategy'] = 0    
     for index, row in deal.iterrows():
+        
         deal.loc[index, 'strategy'] = order[order['order_id'] == row.order_id]['strategy'].values[0]
 
     return deal
 
 
 def save2db(df: pd.DataFrame, table: str):
-    cli = TSDBClient(
-        host=EnvConfig.DB_HOST,
-        port=EnvConfig.DB_PORT,
-        user=EnvConfig.DB_USER,
-        password=EnvConfig.DB_PASSWORD,
-        db=EnvConfig.DB_DATABASE,
-    )
+    
     if table == "dealer.orders":
         date_ = "order_date"
     else:
